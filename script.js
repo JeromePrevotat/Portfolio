b48ed67b7921181c6e31f3af7ccc9191fc7f8d86
Jeeej <jeeej@localhost.localdomain>
2019-01-31 11:12:04 +0100

    Added redirection to github projects

---
+++ script.js
@@ -0,0 +1,27 @@
+let init, dom_strings;
+{
+    dom_strings = {
+        btn_source_code: ".btn-source"
+    };
+
+    let redirect = function(event){
+        let childs, project_name, redirect_to;
+        childs = event.target.parentNode.parentNode.childNodes;
+        project_name = childs[1].textContent.trim();
+        redirect_to = `https://github.com/jeeej/${project_name}`;
+        if(window.location.href == 'https://jeeej.github.io' || window.location.href == `file:///home/jeeej/Portfolio/index.html`)
+            window.location.href=redirect_to;
+    };
+    let setup_event_listener = function(){
+        let download_buttons;
+        download_buttons = document.querySelectorAll(dom_strings.btn_source_code);
+        for (let i = 0; i < download_buttons.length; i++){
+            download_buttons[i].addEventListener('click', redirect);
+        }
+    };
+    init = function(){
+            setup_event_listener();
+    };
+}
+
+init();
