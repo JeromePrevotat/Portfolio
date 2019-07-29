ff25cb0c77bd30c26a9a96226fd0feab6a8e8974
Jeeej <jeeej@localhost.localdomain>
2019-07-29 23:59:50 +0200

    Top part

---
+++ portfolio/website/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+class Project(models.Model):
+    project_name = models.CharField(max_length=50)
+    skills = models.TextField()
+    img = models.ImageField()
+
+    def add_project():
+        self.save()
+
+    def __str__(self):
+        return self.project_name
