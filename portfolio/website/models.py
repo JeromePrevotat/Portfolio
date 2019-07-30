ef2ba621a7a376a91d3bdcbe7c025d6448a7dd2f
Jeeej <jeeej@localhost.localdomain>
2019-07-30 21:28:35 +0200

    trying to fix PIL error

--- portfolio/website/models.py
+++ portfolio/website/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.urls import reverse
+import Image
 
 # Create your models here.
 class Project(models.Model):
     project_name = models.CharField(max_length=50)
     description = models.TextField()
-    img = models.ImageField(upload_to='')
+    img = models.FileField(upload_to='')
 
     def add_project():
         self.save()
