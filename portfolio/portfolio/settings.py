bce9ec023f93768f418e8e74edd35c6a88c1b9e3
Jeeej <jeeej@localhost.localdomain>
2019-07-31 00:22:33 +0200

    debug set to false

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -23,7 +23,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'website/templates/website')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['Jprevotat.eu.pythonanywhere.com',]
 
