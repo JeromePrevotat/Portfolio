96eb687d7da2f7a9c945f26d1178f8ddf6e18a05
Jeeej <jeeej@localhost.localdomain>
2019-09-28 17:45:00 +0200

    Modal for project description added

--- portfolio/website/static/website/js/script.js
+++ portfolio/website/static/website/js/script.js
@@ -1,44 +1,32 @@
 let init, dom_strings;
 {
     dom_strings = {
-        btn_source_code: ".btn-source",
-        top_text: ".top-text"
+        project_title: ".project-name",
+        project_icon: ".project-icon",
     };
 
-    let redirect = function(event){
-        let childs, project_name, redirect_to;
-        childs = event.target.parentNode.parentNode.childNodes;
-        project_name = childs[1].textContent.trim();
-        redirect_to = `https://github.com/jeeej/${project_name}`;
-        console.log(redirect_to);
-        if(window.location.href == 'https://jeeej.github.io' || window.location.href == `file:///home/jeeej/Portfolio/index.html`){
-            console.log('IF');
-            window.location.href=redirect_to;
-        }
+    let build_modal = function(event){
+        //Get project infos
+        let project_infos;
+        project_infos = event.target.parentNode.parentNode.parentNode.childNodes;
+        //Update modal infos with project infos
+        $('#project-modal').on('show.bs.modal', function (event) {
+            var modal = $(this)
+            modal.find(".modal-title").text(project_infos[1].textContent.trim());
+            modal.find(".modal-description").text(project_infos[3].textContent.trim());
+        })
+        //Display modal
+        $(document.querySelectorAll(".modal")).modal();
     };
+
     let setup_event_listener = function(){
-        let download_buttons;
-        download_buttons = document.querySelectorAll(dom_strings.btn_source_code);
-        for (let i = 0; i < download_buttons.length; i++){
-            download_buttons[i].addEventListener('click', redirect);
-        }
+        let all_projects = document.querySelectorAll(dom_strings.project_icon);
+        for (let i = 0; i < all_projects.length; i++){
+            all_projects[i].addEventListener('click', build_modal);
+        };
     };
-    let init_text = function(){
-        let text;
-        text = "Yolooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo" +
-        "ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo" +
-        "ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo" +
-        "ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo" +
-        "ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo" +
-        "ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo" +
-        "ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo" +
-        "ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo" +
-        "ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo" +
-        "oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo";
-        document.querySelector(dom_strings.top_text).insertAdjacentHTML('beforeend', text);
-    };
+
     init = function(){
-        init_text();
         setup_event_listener();
     };
 }
