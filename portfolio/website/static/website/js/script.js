8753651fb7cd7ff87ed01ebfdd3f8dafe024ef06
Jeeej <jeeej@localhost.localdomain>
2019-09-30 18:10:49 +0200

    modals done

--- portfolio/website/static/website/js/script.js
+++ portfolio/website/static/website/js/script.js
@@ -1,30 +1,21 @@
 let init, dom_strings;
 {
     dom_strings = {
-        project_title: ".project-name",
-        project_icon: ".project-icon",
+        modal_toggler: ".modal-toggler",
     };
 
-    let build_modal = function(event){
-        //Get project infos
-        let project_infos;
-        project_infos = event.target.parentNode.parentNode.parentNode.parentNode.childNodes;
-        console.log(project_infos);
+    let build_modal = function(project_name, project_description, event){
         //Update modal infos with project infos
         $('#project-modal').on('show.bs.modal', function (event) {
-            var modal = $(this)
-            modal.find(".modal-title").text(project_infos[1].textContent.trim());
-            modal.find(".modal-description").text(project_infos[3].textContent.trim());
+            document.querySelector(".modal-title").textContent = project_name;
+            document.querySelector(".modal-description").textContent = project_description;
         })
-        console.log(project_infos[3].textContent.trim());
-        //Display modal
-        $(document.querySelectorAll(".modal")).modal();
     };
 
     let setup_event_listener = function(){
-        let all_projects = document.querySelectorAll(dom_strings.project_icon);
+        let all_projects = document.querySelectorAll(dom_strings.modal_toggler);
         for (let i = 0; i < all_projects.length; i++){
-            all_projects[i].addEventListener('click', build_modal);
+            all_projects[i].addEventListener("click", build_modal.bind(null, all_projects[i].dataset.name, all_projects[i].dataset.description), false);
         };
     };
 
